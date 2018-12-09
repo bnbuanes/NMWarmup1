19450b62ddcc472fe36247b4ad4085e38213f6b7
Baste Nesse Buanes <baste@rain-games.com>
2018-12-09 15:33:04 +0100

    That danger zone thing is pretty cool

--- Assets/Scripts/DangerZoneListener.cs
+++ Assets/Scripts/DangerZoneListener.cs
@@ -1,15 +1,41 @@
-﻿using FMODUnity;
+﻿using System;
+using System.Collections.Generic;
+using FMODUnity;
 using UnityEngine;
 
 public class DangerZoneListener : MonoBehaviour {
 
-    public Transform dangerZone;
     public StudioEventEmitter dangerEmitter;
 
     private Collider[] cols = new Collider[10];
 
     private void Update() {
-        Physics.OverlapSphereNonAlloc(transform.position, 10f, cols);
+        var numHits = Physics.OverlapSphereNonAlloc(transform.position, 10f, cols);
+
+        Transform dangerZone;
+        if (numHits > 0) {
+            Array.Sort(cols, 0, numHits, new CompareByDistance { baseTransform = transform });
+            dangerZone = cols[0].transform;
+        }
+        else {
+            dangerZone = null;
+        }
+
+
+        var target = dangerZone == null ? transform.position : dangerZone.position;
+
+        dangerEmitter.transform.position = Vector3.Lerp(dangerEmitter.transform.position, target, Time.deltaTime * 5f);
+    }
+
+    private class CompareByDistance : IComparer<Collider> {
+        public Transform baseTransform;
+        
+        public int Compare(Collider x, Collider y) {
+            var xPos = x.transform.position;
+            var yPos = y.transform.position;
+
+            return Vector3.Distance(xPos, baseTransform.position).CompareTo(Vector3.Distance(yPos, baseTransform.position));
+        }
     }
 }
 
