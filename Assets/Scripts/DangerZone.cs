5cdf1a0b39e912f2f39d9c3e27c2096f7016ffe8
Baste Nesse Buanes <baste@rain-games.com>
2018-12-09 15:52:25 +0100

    More interesting sound scene thing

---
+++ Assets/Scripts/DangerZone.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class DangerZone : MonoBehaviour {
+    private MeshRenderer mr;
+
+    private static Material defaultMat;
+    private static Material closestMat;
+
+    private void Awake() {
+        mr = GetComponent<MeshRenderer>();
+        if (defaultMat == null) {
+            defaultMat = mr.sharedMaterial;
+            closestMat = new Material(defaultMat);
+            closestMat.color = Color.red;
+        }
+    }
+
+    public void SetClosest() => mr.sharedMaterial = closestMat;
+    public void SetNotClosest() => mr.sharedMaterial = defaultMat;
+}
--- Assets/Scripts/DangerZoneListener.cs
+++ Assets/Scripts/DangerZoneListener.cs
@@ -1,35 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
-using FMODUnity;
 using UnityEngine;
+using FMODUnity;
 
 public class DangerZoneListener : MonoBehaviour {
-
     public StudioEventEmitter dangerEmitter;
+    public LayerMask layermask;
+    public float lerpSpeed = 5f;
 
-    private Collider[] cols = new Collider[10];
+    private readonly Collider[] cols = new Collider[10];
+    private DangerZone dangerZone;
 
     private void Update() {
-        var numHits = Physics.OverlapSphereNonAlloc(transform.position, 10f, cols);
-
-        Transform dangerZone;
+        var numHits = Physics.OverlapSphereNonAlloc(transform.position, 10f, cols, layermask);
+        Debug.Log(numHits);
         if (numHits > 0) {
             Array.Sort(cols, 0, numHits, new CompareByDistance { baseTransform = transform });
-            dangerZone = cols[0].transform;
+
+            var old = dangerZone;
+            dangerZone = cols[0].transform.GetComponent<DangerZone>();
+
+            if (old != dangerZone) {
+                if(old != null)
+                    old.SetNotClosest();
+                dangerZone.SetClosest();
+            }
+
         }
         else {
-            dangerZone = null;
+            return;
         }
 
+        var target = dangerZone.transform.position;
 
-        var target = dangerZone == null ? transform.position : dangerZone.position;
-
-        dangerEmitter.transform.position = Vector3.Lerp(dangerEmitter.transform.position, target, Time.deltaTime * 5f);
+        dangerEmitter.transform.position = Vector3.Lerp(dangerEmitter.transform.position, target, Time.deltaTime * lerpSpeed);
     }
 
     private class CompareByDistance : IComparer<Collider> {
         public Transform baseTransform;
-        
+
         public int Compare(Collider x, Collider y) {
             var xPos = x.transform.position;
             var yPos = y.transform.position;
@@ -38,4 +47,3 @@ public class DangerZoneListener : MonoBehaviour {
         }
     }
 }
-
