686525c0b154f2ce7255c6e3c69c546fa5e642d9
spikar1 <Steffen.fureli@gmail.com>
2018-12-09 15:32:31 +0100

    Made player better

--- Assets/SteffenStuff/PlayerController.cs
+++ Assets/SteffenStuff/PlayerController.cs
@@ -11,6 +11,8 @@ public class PlayerController : MonoBehaviour {
 
     public Transform horBase, verBase;
 
+    float verX = 0;
+
     private void Start() {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
@@ -25,5 +27,25 @@ public class PlayerController : MonoBehaviour {
 
         horBase.Rotate(Vector3.up, mouseInput.x);
         verBase.Rotate(Vector3.right, -mouseInput.y);
+
+        verX -= mouseInput.y;
+        verX = Mathf.Clamp(verX, -90, 90);
+
+        verBase.rotation = Quaternion.Euler(verX, 0, 0);
+
+        /*
+        float x = verBase.localRotation.eulerAngles.x;
+
+        if (x > 0 && x < 90 && mouseInput.y < 0)
+            verBase.Rotate(Vector3.right, -mouseInput.y);
+
+        if (x < 360 && x > 270 && mouseInput.y > 0)
+            verBase.Rotate(Vector3.right, -mouseInput.y);
+            */
+
+        //verBase.rotation.eulerAngles.x = Mathf.Clamp(verBase.rotation.eulerAngles.x, -90, 90);
+
+
+
     }
 }
